63cb981dcef1afb5ea9dc90e83f5566beee6626f
teodoraruvceski <teodoraruvceski@gmail.com>
2023-01-05 17:13:51 +0100

    5.1.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const   uuid  = require('uuid');
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 
@@ -13,9 +13,10 @@ const { createClient } =require("@supabase/supabase-js");
 const { createLogger, format, transports } = require("winston");
 
 //database pw:12345678910nebojsa
-// const supabaseUrl = 'https://kmvvsovezjeqmtobddtk.supabase.co'
-// const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImttdnZzb3ZlemplcW10b2JkZHRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzI4NDk1ODEsImV4cCI6MTk4ODQyNTU4MX0.O5kOMXcOJr_mfNqoOOoXCkRpbX_ywd5l51pbhdWjlJI"
+const supabaseUrl = 'https://qxvuqmzydpwwqvldclve.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4dnVxbXp5ZHB3d3F2bGRjbHZlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3MjE1NjAwNCwiZXhwIjoxOTg3NzMyMDA0fQ.P5kK_j5vTzKzNcEZOVEkOqIMmAetTFEND7Q7PCTYTnI"
 const supabase = createClient(supabaseUrl, supabaseKey)
+const bankIdentifier="123456";
 const logLevels = {
   fatal: 0,
   error: 1,
@@ -33,7 +34,11 @@ const logger = createLogger({
   exceptionHandlers: [new transports.File({ filename: "exceptions.log" })],
   rejectionHandlers: [new transports.File({ filename: "rejections.log" })],
 });
-app.listen(8000, () => console.log(`Server Started on ${8000}`));
+app.listen(8000, () => {
+  console.log(`Server Started on ${8000}`);
+  
+}
+  );
 
 
 //poslati zahtjev za placanjem sa psp, zapravo odabir nacina placanja preko psp pogadja ovaj endpoint
@@ -59,18 +64,48 @@ app.listen(8000, () => console.log(`Server Started on ${8000}`));
 //   payment_url,
 //   payment_id
 // }
-app.get('/start-payment', jsonParser, async(req, res) => 
+app.post('/start-payment', jsonParser, async(req, res) => 
 {
+  console.log("start payment");
+  // const paymentInfo={
+  //   merchant_id:merchantBankId,
+  //   merchant_password:merchanBankPassword,
+  //   amount:total,
+  //   merchant_order_id:paymentId,
+  //   merchant_timestamp:Math.round(+new Date()/1000),
+  //   success_url:pspSuccessUrl,
+  //   failed_url:pspFailedUrl,
+  //   error_url:pspErrorUrl
+  // }
+  const payment_info=req.body;
+  try{
+    const id= uuid.v4();
+    const merchant_id= payment_info.merchant_id;
+    const timestamp= payment_info.merchant_timestamp;
+    const amount= Number(payment_info.amount);
+    const completed= false;
+    const success_url=payment_info.success_url;
+    const failed_url=payment_info.failed_url;
+    const error_url=payment_info.error_url;
+    const {data,error}= await supabase
+    .from('transactions')
+    .insert(
+      {id:id,merchant_id:merchant_id,timestamp:timestamp,amount:amount,success_url:success_url,failed_url:failed_url,error_url:error_url,completed:completed}
+      )
+  .single();
+  
+  
+  console.log(data);
+  res.send("url fronta banke");
     //provjeriti postoji li taj korisnik banke, da li je okej, ako jeste kreirati transakciju i njen id i cuvati podatke u neku tabelu
-    //vratitit url fronta banke i id transakcije
-    try{
-       
-      }
-      catch(e)
-      {
-        console.log(e);
-      }
-    
+  //vratitit url fronta banke i id transakcije
+  }
+  catch(e)
+  {
+    console.log(e);
+  }
+ 
+ 
 });
 
 //kada se popuni forma podacima za placanje gadja se ovaj endpoint
@@ -83,6 +118,15 @@ app.get('/start-payment', jsonParser, async(req, res) =>
 // }
 app.get('/finish-payment', jsonParser, async(req, res) => 
 {
+  //TEODORA 5.1.2023 17:12  ->> 
+    // nije mi jasno sta je acquirer timestamp
+    //Kako je meni logicno: metoda koju smo zaj napravili upise u bazu i vrati pspu link do bank fronta...onda se redirektuje na taj url i tamo
+    // popunjava info: pan,csc,exp_date,name....e sad nama treba id od platioca al kako ja da znam svoj id kad placam nesto...to je prob al dobro mozda nam ne treba nz
+    //trebalo bi da mi taj id nadjemo preko pana al aj nz...
+    //zatim te info koje sam navela dodju u ovu metodu finish payment, trazi se taj pan u bazi ove banke ako se nadje proverava se balans, ako ima sredstava skine mu se,
+    //mercentu se doda i redirektuje se na one uspesne urlove i stavi se u bazi da je transakcija completed
+    //problem je sto se ovo moje ne poklapa sa ovim sto si ispod napisao a verujem da si iz specifikacije pisao
+
   // Na sajtu banke prodavca, kupac unosi PAN, security code, card holder name i datum do kada kartica važi. 
   // Vrši se provera podataka.
   // Ukoliko je banka prodavca ista kao i banka kupca, vrši se provera raspoloživih sredstava na računu kupca, 
